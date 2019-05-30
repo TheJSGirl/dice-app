d12cf3a8fdfef14fa05148cf6bbcd04893c4677f
priyanka <prinegi100@gmail.com>
2019-05-30 19:55:18 +0530

    rendered dice icons

--- src/Dice.jsx
+++ src/Dice.jsx
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 
 class Dice extends Component {
     render () {
-        return <h1>Dice!</h1>
+        return <i className={`fas fa-dice-${this.props.face}`}></i>
     }
 }
+
+export default Dice;
